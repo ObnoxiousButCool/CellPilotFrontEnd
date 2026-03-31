fabfa538dccb8859e65fa75a44931bc4d284c6eb
ObnoxiousButCool <shloka.kulkarni@technossus.com>
2026-03-31 16:57:45 +0530

    Adding chat API. need to make backned API for this

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,5 +1,33 @@
+import { useEffect } from "react";
+
 function Chat() {
-  return <h2>Chat Page</h2>;
+  const token = sessionStorage.getItem("access_token");
+
+  useEffect(() => {
+    const callBackend = async () => {
+      try {
+        const res = await fetch("http://localhost:8000/test", {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        const data = await res.json();
+        console.log("Backend response:", data);
+      } catch (err) {
+        console.error("Error calling backend:", err);
+      }
+    };
+
+    callBackend();
+  }, []);
+
+  return (
+    <div>
+      <h2>Chat Page</h2>
+    </div>
+  );
 }
 
 export default Chat;
